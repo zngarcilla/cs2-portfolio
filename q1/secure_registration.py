ee18f75d85b2cfa1321ae26e2d2ab4f0d084d892
zngarcilla <zngarcilla@brc.pshs.edu.ph>
2026-09-17 20:20:29 +0800

    Update secure_registration.py

--- q1/secure_registration.py
+++ q1/secure_registration.py
@@ -1,1 +1,20 @@
+name = input("Enter name:")
+if name == "":
+  print("Student name is required.")
+  name_valid = False
+else:
+  name_valid = True
 
+club = input("Enter club name:").title()  
+if club in ["Robotics", "Science", "Mathematics", "Programming"]:
+  club_valid = True
+else:
+  print("Please choose a valid club.")
+  club_valid = False
+
+attendance = input("Attendance status:").title()  
+if attendance in ["Absent", "Late", "Present"]:
+  attendance_valid = True
+else:
+  print("Please choose a proper status.")
+  attendance_valid = False
